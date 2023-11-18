a9d349b4d4d64222021e71eb61a888f0581e997b
ElayarajaK <kelaya5@gmail.com>
2023-11-18 11:57:43 +0530

    commit

---
+++ 18-11/App1.py
@@ -0,0 +1,24 @@
+'''
+type casting
+   1. implicit conversion (PVM taking responsiblity of conversion Automatically )
+   2. Explicit conversion
+'''
+'''Explicit'''
+print(bool(100))
+print(int(False))
+
+
+if(""):
+ print("Your Eligible")
+else:
+ print("Not Eligible")
+
+
+
+'''
+Age > 18
+     you are Eligible for voting
+    Age < 18 
+     yor are not eligible
+'''
+
